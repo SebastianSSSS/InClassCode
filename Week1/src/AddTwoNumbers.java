7c2119a7b1ce7ef7ce9c3f24b8b6c31dd1f3cfc8
KwanR <12602991+bitkwan@users.noreply.github.com>
2020-02-25 13:48:45 -0800

    Week 1

---
+++ Week1/src/AddTwoNumbers.java
@@ -0,0 +1,17 @@
+/*
+ * Sebastian Sang
+ * Java A170
+ * 01/30/2020
+ * Hello World
+ * The project of first week
+ */
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		int num1 = 1, num2 = 100, sum;
+		sum = num1 + num2;
+		System.out.println("Sum: "+sum);
+	}
+
+}
---
+++ Week1/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * Sebastian Sang
+ * Java A170
+ * 01/30/2020
+ * Hello World
+ * The project of first week
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hey, it\'s Sebastian");
+		System.out.print("I\'m taking Java A170");
+		//difference between printing with quotation marks and without
+		System.out.println("\n1 + 1 =");
+		System.out.println(1 + 1);
+	}
+
+}
