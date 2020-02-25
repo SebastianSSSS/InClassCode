4d2c0d09e8b95fe458806b4b040326decec9bb8f
KwanR <12602991+bitkwan@users.noreply.github.com>
2020-02-25 13:49:17 -0800

    Week 4

---
+++ Week4/src/Program.java
@@ -0,0 +1,11 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		String letters = "abc";
+		char letter2 = letters.charAt(1);
+		System.out.println(letter2);
+		
+	}
+
+}
---
+++ Week4/src/SubStringDemo.java
@@ -0,0 +1,12 @@
+
+public class SubStringDemo {
+
+	public static void main(String[] args) {
+		String str = "Example String";
+		System.out.println("Value - " + str.substring(0,7));
+		System.out.println("Value - " + str.substring(8));
+		System.out.println("Value - " + str.substring(14));
+
+	}
+
+}
